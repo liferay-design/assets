81fd7c2ac657b43109d537313e7ddfaf006f8863
plhnk <paul.hanaoka@gmail.com>
2018-09-22 00:06:43 -0700

    create homepage

--- README.md
+++ README.md
@@ -18,10 +18,18 @@ Quickly develop consistent svg animations, and share improvements along the way.
 
 ### Necessities:
 
-1. A WeDeploy account
-1. A fork and clone of this repo
+1. A [WeDeploy account](https://console.wedeploy.com/projects/liferaydotdesign/services/assets/logs)
+1. A [fork and clone](TODO:write-github-guidelines) of this repo
+
+### How to:
+
+1. add, edit, or remove files
+1. to see your changes live at a url &mdash; deploy your project!
+   1. enter `we deploy -p liferaydotdesign-dev` into your terminal (note you will need to be a contributor in our wedeploy project 🤓 &mdash; paul will add you!)
+   1. go to `https://dev-assets.liferay.design` in your browser
+1. if you'd like to see your changes live, [send a pull request](https://github.com/liferay-design/assets/compare) to share with the rest of the team!
 
 ### Suggestions:
 
 1. use [VSCode](https://code.visualstudio.com/)
-1. get [the SVG extension](https://marketplace.visualstudio.com/items?itemName=jock.svg&WT.mc_id=vscoderelease-medium-buhollan)
+1. get [the SVG extension](https://marketplace.visualstudio.com/items?itemName=jock.svg&WT.mc_id=vscoderelease-medium-buhollan) to see your changes in realtime
--- gulpfile.js
+++ gulpfile.js
@@ -9,38 +9,61 @@ var browserSync = require("browser-sync").create();
 //         .pipe(gulp.dest('$destinationFolder'))          // Outputs the file in the destination folder
 // })
 
+var paths = {
+    dirs: {
+        dist: {
+            css: 'app/dist/css',
+            html: 'app/dist/'
+        },
+        src: {
+            allFiles: 'app/src/**/*',
+            scss: 'app/src/scss',
+            html: 'app/src/*.html'
+        },
+    },
+    sass: 'app/src/scss/**/*.scss'
+}
+
 gulp.task('browserSync', function () {
     browserSync.init({
         server: {
-            baseDir: 'app'
+            baseDir: 'app/dist'
         },
-        files: [ // filetypes to watch for:
-            '**/*.css',
-            '**/*.js',
-            '**/*.svg',
-            '**/*.xml',
-            '**/*.html'
-            // etc...
-        ]
+        port: 8080,
+        open: false,
+        notify: false,
     });
 });
 
+// build app files
 gulp.task('sass', function () {
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src(paths.sass)
         .pipe(sass())
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest(paths.dirs.dist.css))
         .pipe(browserSync.reload({
             stream: true
         }))
 })
 
-// tasks to watch:
-gulp.task('watch', gulp.series('browserSync', 'sass'), function () {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
+gulp.task('html', function() {
+    return gulp.src(paths.dirs.src.html)
+        .pipe(gulp.dest(paths.dirs.dist.html))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 })
 
-// build tasks
-gulp.task(
-    'build', 
-    gulp.series( 'sass' )
-    );
+gulp.task('app', gulp.parallel('sass', 'html'));
+
+gulp.task('build', gulp.series('app', 'browserSync'));
+
+gulp.task('refresh', function(refreshed) {
+    browserSync.reload()
+    refreshed();
+})
+
+gulp.task('watch', function () {
+    gulp.watch(paths.dirs.src.allFiles, gulp.series('app', 'refresh'));
+})
+
+gulp.task('start', gulp.parallel('build', 'watch'));
